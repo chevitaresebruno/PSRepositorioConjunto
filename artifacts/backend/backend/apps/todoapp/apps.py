eb98d9836769bd9f317acbf9a329cf406a6c1dde
chevitaresebruno <chevitarese.bruno@gmail.com>
2025-07-03 14:42:39 -0300

    Isso é um teste

--- artifacts/backend/backend/apps/todoapp/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class ToDoAppConfig(AppConfig):
-    name  = 'apps.todoapp'
-    label = 'apps_todoapp'
-
-    def ready(self):
-        import apps.todoapp.signals
